162515c346bda7311ff8605388672178f07a2adf
Mykola <mykola.rak.89@gmail.com>
2016-04-18 18:17:23 +0300

    templates

---
+++ leaderboard.js
@@ -0,0 +1,16 @@
+PlayersList = new Mongo.Collection('players');
+
+
+if(Meteor.isClient){
+
+    Template.leaderboard.helpers({
+    	'player': function(){
+        	return PlayersList.find();;
+    	}
+	});
+	
+}
+
+if(Meteor.isServer){
+    
+}
