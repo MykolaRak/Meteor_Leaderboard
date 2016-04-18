53bbd34b20625c0da8b9ffd69401878fa0f43354
Mykola <mykola.rak.89@gmail.com>
2016-04-18 18:59:57 +0300

    db

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,7 @@ if(Meteor.isClient){
 
     Template.leaderboard.helpers({
     	'player': function(){
-        	return PlayersList.find();;
+        	return PlayersList.find({}, { sort: {score: -1, name: 1} });
     	},
     	'selectedClass': function(){
     		var playerId = this._id;
@@ -13,6 +13,10 @@ if(Meteor.isClient){
 		    if(playerId == selectedPlayer){
 		        return "selected"
 		    }
+		},
+		'selectedPlayer': function(){
+		    var selectedPlayer = Session.get('selectedPlayer');
+		    return PlayersList.findOne({ _id: selectedPlayer });
 		}
 	});
 
@@ -20,6 +24,14 @@ if(Meteor.isClient){
 	    'click .player': function(){
 		    var playerId = this._id;
 		    Session.set('selectedPlayer', playerId);
+		},
+		'click .increment': function(){
+		    var selectedPlayer = Session.get('selectedPlayer');
+		    PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5 }});
+		},
+		'click .decrement': function(){
+		    var selectedPlayer = Session.get('selectedPlayer');
+		    PlayersList.update({ _id: selectedPlayer }, { $inc: {score: -5 }});
 		}
 	});
 }
